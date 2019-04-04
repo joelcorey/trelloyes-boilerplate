2265dee46c3caf01e3f4b9e9b2d33cfa097abb7b
joelcorey <joelcorey@fastmail.com>
2019-04-04 11:02:13 -0700

    auth moved to middleware

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { NODE_ENV } = require('./config')
 const winston = require('winston');
 const cardRouter = require('./card/card-router')
 const listRouter = require('./list/list-router')
-
+const authMiddleware = require('./auth')
 const app = express()
 
 const morganOption = (process.env.NODE_ENV === 'production')
@@ -18,19 +18,9 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware
-  next()
-})
-
+app.use(authMiddleware)
 app.use(cardRouter)
+app.use(listRouter)
 
 app.use(function errorHandler(error, req, res, next) {
   let response
@@ -41,10 +31,6 @@ app.use(function errorHandler(error, req, res, next) {
     response = { message: error.message, error }
   }
   res.status(500).json(response)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!')
 })
 
 module.exports = app
